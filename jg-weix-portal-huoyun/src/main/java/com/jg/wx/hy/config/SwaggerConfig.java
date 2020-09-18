5662a0fc979d3957065d59255f789fd43601b20a
Shkin1 <240493030@qq.com>
2020-09-18 16:28:37 +0800

    style

--- jg-weix-portal-huoyun/src/main/java/com/jg/wx/hy/config/SwaggerConfig.java
+++ jg-weix-portal-huoyun/src/main/java/com/jg/wx/hy/config/SwaggerConfig.java
@@ -16,7 +16,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-//@ComponentScan(basePackages="com.jg.wx.hy.controller")
 public class SwaggerConfig {
 
     @Bean
